8861ad5e34d702835de884aa044cd1ea56d1ff31
Rafael Badaró <rafabadaro.15@gmail.com>
2020-03-15 12:39:44 -0300

    Script e csv dos repositórios do criador do python

--- script.py
+++ script.py
@@ -2,8 +2,7 @@ import requests
 import time
 import csv
 
-headers = {"Authorization": "token ###"}
-
+headers = {"Authorization": "token 6207e9b60742649092a01fc229a44ee4746a3672"}
 
 
 def run_query(query):
@@ -19,91 +18,91 @@ def run_query(query):
         raise Exception("Query failed to run by returning code of {}. {}".format(
             request.status_code, query))
 
-#Minera os repositórios
+# Minera os repositórios
+
+
 def mine(first, interval):
-  endCursor = "null"  # Proxima pagina
-  nodes = []  # Resultados da query
+    endCursor = "null"  # Proxima pagina
+    nodes = []  # Resultados da query
 
-  for x in range(interval):
+    # for x in range(interval):
     # GraphQL query
-    query = """
-    {
-    search(query: "stars:>100", type: REPOSITORY, first: %s, after:%s) {
-      pageInfo {
-        endCursor
-      }
-      nodes {
-        ... on Repository {
-          nameWithOwner
-          url
-          createdAt
-          updatedAt
-          pullRequests(states: MERGED) {
-            totalCount
-          }
-          releases {
-            totalCount
-          }
-          primaryLanguage{
-            name
-          }
-          numeroTotalIssues: issues {
-            totalCount
-          }
-          numeroTotalIssuesClosed: issues(states: CLOSED) {
-            totalCount
-          }
-        }
-      }
-    }
-    }
-    """ % (first, endCursor)
 
-      # O resultado da query que contem a proxima pagina e os nodes
+    query = """
+          {
+            user(login: "gvanrossum") {
+              repositories(first: 23) {
+                nodes {
+                  nameWithOwner
+                  url
+                  stargazers {
+                    totalCount
+                  }
+                  watchers {
+                    totalCount
+                  }
+                  forks {
+                    totalCount
+                  }
+                  releases {
+                    totalCount
+                  }
+                  createdAt
+                  primaryLanguage {
+                    name
+                  }
+                }
+              }
+            }
+          }
+          """
+
+    # O resultado da query que contem a proxima pagina e os nodes
     queryResult = run_query(query)
-    querySize = len(queryResult['data']['search']['nodes'])
+    querySize = len(queryResult['data']['user']['repositories']['nodes'])
     for y in range(querySize):
-          # Salva os nodes no array de nodes
-          nodes.append(queryResult["data"]["search"]["nodes"][y])
-          # Pega o endCursor aka proxima pagina
-          endCursor = '"{}"'.format(
-              queryResult["data"]["search"]["pageInfo"]["endCursor"])
-  
-  return nodes
+        # Salva os nodes no array de nodes
+        nodes.append(queryResult['data']['user']['repositories']['nodes'][y])
+        # Pega o endCursor aka proxima pagina
+        # endCursor = '"{}"'.format(
+        #     queryResult["data"]["search"]["pageInfo"]["endCursor"])
+
+    return nodes
 
 # Escreve em um arquivo csv
+
+
 def writeCsv(nodes):
-  with open("/Users/Rafael/Desktop/labex/repos.csv", 'w') as new_file:
+    with open("/Users/Rafael/Desktop/labex2/repos.csv", 'w') as new_file:
 
-    fnames = [
-        'name_with_owner',
-        'url',
-        'created_at',
-        'updated_at',
-        'merged_pull_requests',
-        'releases',
-        'primary_language',
-        'total_issues',
-        'total_issues_closed']
+        fnames = [
+            'name_with_owner',
+            'url',
+            'stars',
+            'watchers',
+            'forks',
+            'releases',
+            'created_at',
+            'primary_language'
+        ]
 
-    csv_writer = csv.DictWriter(new_file, fieldnames=fnames)
-    csv_writer.writeheader()
-    for node in nodes:
-        csv_writer.writerow(
-            {
-                'name_with_owner': node['nameWithOwner'],
-                'url': node['url'],
-                'created_at': node['createdAt'],
-                'updated_at': node['updatedAt'],
-                'merged_pull_requests': node['pullRequests']['totalCount'],
-                'releases': node['releases']['totalCount'],
-                'primary_language': node['primaryLanguage']['name'] if node['primaryLanguage']!= None else 'null',
-                'total_issues': node['numeroTotalIssues']['totalCount'],
-                'total_issues_closed': node['numeroTotalIssuesClosed']['totalCount'],
-            })
+        csv_writer = csv.DictWriter(new_file, fieldnames=fnames)
+        csv_writer.writeheader()
+        for node in nodes:
+            csv_writer.writerow(
+                {
+                    'name_with_owner': node['nameWithOwner'],
+                    'url': node['url'],
+                    'stars': node['stargazers']['totalCount'],
+                    'watchers': node['watchers']['totalCount'],
+                    'forks': node['forks']['totalCount'],
+                    'releases': node['releases']['totalCount'],
+                    'created_at': node['createdAt'],
+                    'primary_language': node['primaryLanguage']['name'] if node['primaryLanguage'] != None else 'null',
+                })
 
-    print('Arquivo csv gerado com sucesso!')
+        print('Arquivo csv gerado com sucesso!')
 
 
-nodes = mine(20, 50)
+nodes = mine(0, 1)
 writeCsv(nodes)
