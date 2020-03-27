a6870f8f9a2559d1768f1bd1646afa20662f211c
Rafael Badaró <rafabadaro.15@gmail.com>
2020-03-27 20:22:08 -0300

    Ajustando script para buscar os 1000 repositorios e criando um arquivo de teste chamado 'mil_repos.csv'

---
+++ SPRINT_I/script_graphql.py
@@ -0,0 +1,112 @@
+import requests
+import time
+import csv
+
+headers = {"Authorization": "token xxxx"}
+
+
+def run_query(query):
+    request = requests.post('https://api.github.com/graphql',
+                            json={'query': query}, headers=headers)
+    while (request.status_code == 502):
+        time.sleep(2)
+        request = requests.post(
+            'https://api.github.com/graphql', json={'query': query}, headers=headers)
+    if request.status_code == 200:
+        return request.json()
+    else:
+        raise Exception("Query failed to run by returning code of {}. {}".format(
+            request.status_code, query))
+
+# Minera os repositórios
+
+
+def mine(first, interval):
+    endCursor = "null"  # Proxima pagina
+    nodes = []  # Resultados da query
+
+    # for x in range(interval):
+    # GraphQL query
+    for x in range(interval):
+        query = """
+            {
+                search(query: "stars:>100 language:Python", type: REPOSITORY, first: %s, after:%s) {
+                  pageInfo {
+                    endCursor
+                  }
+                  nodes {
+                    ... on Repository {
+                      nameWithOwner
+                      url
+                      stargazers {
+                        totalCount
+                      }
+                      watchers {
+                        totalCount
+                      }
+                      forks {
+                        totalCount
+                      }
+                      releases {
+                        totalCount
+                      }
+                      createdAt
+                      primaryLanguage {
+                        name
+                      }
+                    }
+                  }
+                }
+              }
+              """ % (first, endCursor)
+
+        # O resultado da query que contem a proxima pagina e os nodes
+        queryResult = run_query(query)
+        queryResultData = queryResult['data']['search']['nodes']
+        querySize = len(queryResultData)
+        for y in range(querySize):
+            # Salva os nodes no array de nodes
+            if queryResultData[y]['primaryLanguage'] != None and queryResultData[y]['primaryLanguage']['name'] == 'Python':
+                nodes.append(queryResultData[y])
+            # Pega o endCursor aka proxima pagina
+            endCursor = '"{}"'.format(queryResult["data"]["search"]["pageInfo"]["endCursor"])
+
+    return nodes
+
+# Escreve em um arquivo csv
+
+
+def writeCsv(nodes):
+    with open("/Users/Rafael/Desktop/labex2/SPRINT_I/csv_files/mil_repos.csv", 'w') as new_file:
+
+        fnames = [
+            'name_with_owner',
+            'url',
+            'stars',
+            'watchers',
+            'forks',
+            'releases',
+            'created_at',
+            'primary_language'
+        ]
+
+        csv_writer = csv.DictWriter(new_file, fieldnames=fnames)
+        csv_writer.writeheader()
+        for node in nodes:
+            csv_writer.writerow(
+                {
+                    'name_with_owner': node['nameWithOwner'],
+                    'url': node['url'],
+                    'stars': node['stargazers']['totalCount'],
+                    'watchers': node['watchers']['totalCount'],
+                    'forks': node['forks']['totalCount'],
+                    'releases': node['releases']['totalCount'],
+                    'created_at': node['createdAt'],
+                    'primary_language': node['primaryLanguage']['name'] if node['primaryLanguage'] != None else 'null',
+                })
+
+        print('Arquivo csv gerado com sucesso!')
+
+
+nodes = mine(100, 2)
+writeCsv(nodes)
